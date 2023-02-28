c94ce113cbe70b5aad0ac0167cb3a1c72be0a2ac
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-02-28 16:01:29 +0100

    add basic views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from listings.models import Band
+from listings.models import Listing
+
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
+
+
+def listings(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings/listing.html', {'listings': listings})
+
+
+def contact(request):
+    return render(request, 'listings/contact.html')
+
+
+def about(request):
+    return render(request, 'listings/about.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from listings import views
+
 urlpatterns = [
+    path("", views.hello),
     path("admin/", admin.site.urls),
+    path("hello/", views.hello),
+    path("listings/", views.listings),
+    path("contact/", views.contact),
+    path("about/", views.about),
 ]
