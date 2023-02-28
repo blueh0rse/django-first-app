7c1eeb313823426e4bdcaa7fc85462a1ea9aa49b
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-02-28 16:02:16 +0100

    Change admin entity view

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from listings.models import Band
+from listings.models import Listing
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+admin.site.register(Band, BandAdmin)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'sold', 'type', 'band')
+
+admin.site.register(Listing, ListingAdmin)
